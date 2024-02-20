5686f32563fedc080744dbb52c14853161ac018c
Nadi Lin Latt <nadilinlatt@Nadis-MacBook.local>
2024-02-20 20:28:51 +0630

    dart_oop

---
+++ Getter/example3.dart
@@ -0,0 +1,31 @@
+class NoteBook {
+  // Private properties
+  String _name;
+  double _prize;
+
+  // Constructor
+  NoteBook(this._name, this._prize);
+
+  // Getter to access private property _name
+  String get name {
+    if (_name == "") {
+      return "No Name";
+    }
+    return this._name;
+  }
+
+  // Getter to access private property _prize
+  double get price {
+    return this._prize;
+  }
+}
+
+void main() {
+  // Create an object of NoteBook class
+  NoteBook nb = new NoteBook("Apple", 1000);
+  print("First Notebook name: ${nb.name}");
+  print("First Notebook price: ${nb.price}");
+  NoteBook nb2 = new NoteBook("", 500);
+  print("Second Notebook name: ${nb2.name}");
+  print("Second Notebook price: ${nb2.price}");
+}
---
+++ Setter/example2.dart
@@ -0,0 +1,33 @@
+class NoteBook {
+  // Private properties
+  String? _name;
+  double? _prize;
+
+  // Setter to update the value of name property
+  set name(String name) => _name = name;
+
+  // Setter to update the value of price property
+  set price(double price) {
+    if (price < 0) {
+      throw Exception("Price cannot be less than 0");
+    }
+    this._prize = price;
+  }
+
+  // Method to display the values of the properties
+  void display() {
+    print("Name: $_name");
+    print("Price: $_prize");
+  }
+}
+
+void main() {
+  // Create an object of NoteBook class
+  NoteBook nb = new NoteBook();
+  // setting values to the object using setter
+  nb.name = "Dell";
+  nb.price = 250;
+
+  // Display the values of the object
+  nb.display();
+}
